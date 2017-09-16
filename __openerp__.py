0c904e879d2599c53b3ec163d8ba5821ebbe3aef
gibil <jrevilla55@gmail.com>
2017-09-16 17:10:41 -0500

    ..

--- __openerp__.py
+++ __openerp__.py
@@ -109,9 +109,8 @@
 
 
 		# ----------------------------------------------------------- Security ------------------------------------------------------
-		#'data/base_data_users.xml',
-		'data/base_data_users_buf.xml',
-		#'data/base_data_users_phy.xml',
+		#'data/base_data_users_buf.xml',
+
 
 
 

c82f6bbf97d809e7301bc185487eeb78dd885ff9
gibil <jrevilla55@gmail.com>
2017-10-03 15:41:01 -0500

    ..

--- models/treatment.py
+++ models/treatment.py
@@ -1924,43 +1924,127 @@ class Treatment(models.Model):
 		print 'CRUD - Treatment - Write'
 		print 
 		print vals
-		#print 
-		#print 
-
-		#if vals['x_doctor'] != False: 
-		#	print vals['x_doctor']
-		#if vals['user_id'] != False: 
-		#	print vals['user_id']
-
-
-
-		print 'patient', self.patient.name
-		print 'physicien', self.physician.name 
-
-		app = self.env['oeh.medical.appointment'].search([
-																('patient', '=', self.patient.name), 
-																('x_type', '=', 'consultation'),
-																('doctor', '=', self.physician.name), 
-																#('x_target', '=', record.x_target),	
-														],
-														order='appointment_date desc',
-														limit=1,
-													)
-		print app 
-
-		if app.id != False:
-			app.treatment = self.id 
-			print 'id', app.id
-			print 'treatment', app.treatment 
-
 
 
 
 		#Write your logic here
 		res = super(Treatment, self).write(vals)
 		#Write your logic here
+
+
+
+
+
+
+
+		print 'patient', self.patient.name
+		print 'physicien', self.physician.name 
+
+
+		app_c = self.env['oeh.medical.appointment'].search([
+															('patient', '=', self.patient.name), 
+															('x_type', '=', 'consultation'),
+															('doctor', '=', self.physician.name), 
+														],
+														order='appointment_date desc',
+														limit=1,
+													)
+		app_p = self.env['oeh.medical.appointment'].search([
+															('patient', '=', self.patient.name), 
+															('x_type', '=', 'procedure'),
+															('doctor', '=', self.physician.name), 
+														],
+														order='appointment_date desc',
+														limit=1,
+													)
+
+		print 'id', self.id
+
+		print app_c 
+		if app_c.id != False:
+			app_c.treatment = self.id 
+			print 'c id', app_c.id
+			print 'treatment', app_c.treatment 
+
+
+		print app_p 
+		if app_p.id != False:
+			app_p.treatment = self.id 
+			print 'p id', app_p.id
+			print 'treatment', app_p.treatment 
+
+
+
+
+
+
+
+
+		return res
+
+
+
+
+
+
+# ----------------------------------------------------------- CRUD ------------------------------------------------------
+
+	@api.model
+	def create(self,vals):
+
+
+
 		print 
+		print 'CRUD - Treatment - Create'
 		print 
+		print vals
+
+
+
+		print 'patient', self.patient.name
+		print 'physicien', self.physician.name 
+
+
+		app_c = self.env['oeh.medical.appointment'].search([
+															('patient', '=', self.patient.name), 
+															('x_type', '=', 'consultation'),
+															('doctor', '=', self.physician.name), 
+														],
+														order='appointment_date desc',
+														limit=1,
+													)
+		app_p = self.env['oeh.medical.appointment'].search([
+															('patient', '=', self.patient.name), 
+															('x_type', '=', 'procedure'),
+															('doctor', '=', self.physician.name), 
+														],
+														order='appointment_date desc',
+														limit=1,
+													)
+
+		print 'id', self.id
+
+		print app_c 
+		if app_c.id != False:
+			app_c.treatment = self.id 
+			print 'c id', app_c.id
+			print 'treatment', app_c.treatment 
+
+
+		print app_p 
+		if app_p.id != False:
+			app_p.treatment = self.id 
+			print 'p id', app_p.id
+			print 'treatment', app_p.treatment 
+
+
+
+
+
+
+		# Put your logic here 
+		res = super(Treatment, self).create(vals)
+		# Put your logic here 
 
 		return res
 

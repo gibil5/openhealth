1745c8e55085712be85207ba3f22285a4d5ad8af
gibil <jrevilla55@gmail.com>
2017-10-17 15:42:38 -0500

    ..

--- models/partner.py
+++ models/partner.py
@@ -55,6 +55,62 @@ class Partner(models.Model):
 
 
 
+	@api.multi
+	def process_name(self, name):  
+
+		arr = name.split(' ')
+
+		second = arr[0] + ' ' + arr[1]
+
+		first = arr[2]
+
+		if len(arr) > 3:
+
+			first = first + ' ' + arr[3]
+
+
+		second = second.upper()
+		first = first.title()
+		full = second + ' ' + first
+
+
+		return full  
+
+
+# ----------------------------------------------------------- CRUD ------------------------------------------------------
+
+	@api.model
+	def create(self,vals):
+
+		print 
+		#print 'jx: begin'
+		print 'jx'
+		print 'Partner - Create - Override'
+		print 
+		#print vals
+		print vals['name']
+
+		#print 'name: ', self.name 
+	
+
+
+		vals['name'] = self.process_name(vals['name'])
+		print vals['name']
+
+
+
+		# Put your logic here 
+		res = super(Partner, self).create(vals)
+		# Put your logic here 
+
+
+		#print 'name: ', self.name 
+		print 
+
+
+		return res
+
+	# CRUD - Create 
 
 
 

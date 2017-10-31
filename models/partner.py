d56669a5b485a9a27bfeeabd3b8056f3033a8499
gibil <jrevilla55@gmail.com>
2017-10-30 19:44:17 -0500

    ..

--- models/partner.py
+++ models/partner.py
@@ -46,12 +46,12 @@ class Partner(models.Model):
 
 
 
-	x_firm = fields.Char(
-		"Razon social", 	
-		)
-	x_ruc = fields.Char(
-		"Ruc", 	
-	)
+	#x_firm = fields.Char(
+	#	"Razon social", 	
+	#	)
+	#x_ruc = fields.Char(
+	#	"Ruc", 	
+	#)
 
 
 

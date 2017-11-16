c4e1d1cf5da21aa6a2840af546a5b0c3d4d809d5
gibil <jrevilla55@gmail.com>
2017-11-16 12:32:43 -0500

    ..

--- __openerp__.py
+++ __openerp__.py
@@ -200,14 +200,14 @@
 
 
 		# Products 
-		'data/odoo_data_products.xml',			# check 
+		'data/prods/odoo_data_products.xml',			# check 
 		
-		'data/odoo_data_services_co2.xml',
-		'data/odoo_data_services_exc.xml',
-		'data/odoo_data_services_m22.xml',
-		'data/odoo_data_services_med.xml',
-		'data/odoo_data_services_cos.xml',
-		'data/odoo_data_services_consult.xml',
+		'data/prods/odoo_data_services_co2.xml',
+		'data/prods/odoo_data_services_exc.xml',
+		'data/prods/odoo_data_services_m22.xml',
+		'data/prods/odoo_data_services_med.xml',
+		'data/prods/odoo_data_services_cos.xml',
+		'data/prods/odoo_data_services_consult.xml',
 
 
 

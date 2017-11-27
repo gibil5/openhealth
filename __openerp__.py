e9f06ea73b29097055e9c7486c579c19cc2b6641
gibil <jrevilla55@gmail.com>
2017-11-27 13:20:11 -0500

    ..

--- __openerp__.py
+++ __openerp__.py
@@ -20,7 +20,7 @@
 
 		Laser - EMR - Min - Ama - Docean - Ooor - Github - Travis - Coverage - Proliant\n
 
-		Last built: 	24 November 2017 - Yey !
+		Last built: 	27 November 2017 - Yey !
 
 		Created: 	 	11 Sep 2016
 

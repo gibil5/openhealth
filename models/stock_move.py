247845dcfa7f05330222c59c604e9f868958baf3
gibil <jrevilla55@gmail.com>
2017-12-04 14:37:07 -0500

    ..

--- models/stock_move.py
+++ models/stock_move.py
@@ -9,5 +9,12 @@ class StockMove(models.Model):
 
 	_description = "Stock Move"
 
-	_order = 'create_date desc'
+	#_order = 'create_date desc'
+	#_order = 'date desc'
+	#_order = 'date_expected desc'
+
+
+	#_name = "stock.move"
+	#_description = "Stock Move"
+	#_order = 'picking_id, sequence, id'
 

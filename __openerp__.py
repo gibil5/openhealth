8acfc5dff769137f8ad547abe4e6c1634b0ab9f5
gibil <jrevilla55@gmail.com>
2017-12-28 16:20:46 -0500

    ..

--- __openerp__.py
+++ __openerp__.py
@@ -7,52 +7,27 @@
 {
 	'name': "Open Health - MIN",
 
+	
 	'summary': """
-		Extension for Odoo-oeHealth. 
-		Treatments. 
 	""",
 
 
 	'description': """
 
-		Clínica Chavarri\n
-
-		Laser - EMR - Min - Ama - Docean - Ooor - Github - Travis - Coverage - Proliant\n
-
-		Last built: 	28 December 2017 - Yey !
-
-		Created: 	 	11 Sep 2016
-
-		Active for: 	15 months !!!
+		MINIMAL
 
 		---
-
-
-
-		This is my first extension for oeHealth.
-
-		It provides the following objects:
-			- Patients,
-			- Treatments,
-			- Evaluations,
-			- Consultations,
-			- Procedures,
-			- Controls,
-			- Sessions,
-			- Services,
-			- Orders,
-			- Appointments,
-			- Calendar, 
-			- ...
 			
 			
 	""",
 
-	'author': "DataMetrics",
-	'website': "http://jrevilla.com/",
-	'category': 'Health',
-	'version': '1.0',
-	
+	'author': "",
+
+	'website': "",
+
+	'category': '',
+
+	'version': '',
 
 	'depends': ['base', 'oehealth', 'base_multi_image'],
 

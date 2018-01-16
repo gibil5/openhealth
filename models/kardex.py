f5e9f81704811e89bcfa6ad395b59c25b6f5f9ec
gibil <jrevilla55@gmail.com>
2018-01-16 14:45:20 -0500

    ..

--- models/kardex.py
+++ models/kardex.py
@@ -16,13 +16,115 @@ class Kardex(models.Model):
 		)
 
 
-	stock_move_ids = fields.One2many(
 
+	stock_move_ids = fields.One2many(
 			#'stock.move', 
 			'openhealth.kardex.move', 
 
 			'kardex_id', 
 		)
+
+
+	product = fields.Many2one(
+			'product.product'
+		)
+
+
+# ----------------------------------------------------------- Generate ------------------------------------------------------
+
+	# Generate 
+	@api.multi 
+	def generate_kardex(self):
+		
+		print 'jx'
+		print 'Generate'
+
+		#self.remove_kardex()
+		
+		#res_id = self.create_kardex()
+
+		self.update_kardex()
+
+
+
+
+
+
+	# Remove 
+	@api.multi 
+	def remove_kardex(self):
+
+		print 'jx'
+		print 'Remove'
+
+		self.stock_move_ids.unlink()
+
+
+
+
+
+
+	# Update 
+	@api.multi 
+	def update_kardex(self):
+
+		print 'jx'
+		print 'Update'
+
+
+		if self.product.name != False: 
+
+
+			product_id = self.product.id 
+
+			kardex_id = self.id 
+
+
+			moves = self.env['stock.move'].search([
+
+															('product_id', '=', product_id),			
+															('state', '=', 'done'),			
+				
+													],
+													#order='start_date desc',
+													#limit=1,
+													)
+			print moves
+
+
+			for move in moves: 
+			
+				print move.name
+				print move.product_id
+				print move.date
+				print move.product_uom_qty
+
+
+				ret = self.stock_move_ids.create({
+																
+																'name': move.name,
+
+																'product_id': move.product_id.id,
+
+																'date': move.date,
+																
+																'product_uom_qty': move.product_uom_qty, 
+
+																'kardex_id': kardex_id,
+
+			#													'price_subtotal': line.price_subtotal,
+			#													'x_date_created': line.create_date,
+																#'move_id': self.x_kardex.id,
+														})
+
+
+
+
+
+
+
+
+
 
 
 

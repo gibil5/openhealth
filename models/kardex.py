661ea3e16b766b2586a9b9c08d7fd28d57f1f296
gibil <jrevilla55@gmail.com>
2018-01-16 20:15:47 -0500

    ..

--- models/kardex.py
+++ models/kardex.py
@@ -6,18 +6,34 @@ from openerp import models, fields, api
 class Kardex(models.Model):
 
 	#_inherit = 'openhealth.process'	
-	#_order = 'write_date desc'
+	
+	_order = 'product'
 
 	_name = 'openhealth.kardex'
 
 
 
 	name = fields.Char(
+
+			compute='_compute_name', 
 		)
+
+
+	@api.multi
+	def _compute_name(self):
+
+		for record in self:
+
+			if record.product.x_name_short != False: 
+				record.name = record.product.x_name_short
+
+
+
 
 
 
 	stock_move_ids = fields.One2many(
+
 			#'stock.move', 
 			'openhealth.kardex.move', 
 
@@ -113,6 +129,12 @@ class Kardex(models.Model):
 
 																'kardex_id': kardex_id,
 
+
+																'location_dest': move.location_dest_id.name, 
+
+																'location': move.location_id.name, 
+
+
 			#													'price_subtotal': line.price_subtotal,
 			#													'x_date_created': line.create_date,
 																#'move_id': self.x_kardex.id,
@@ -134,7 +156,9 @@ class Kardex(models.Model):
 
 	# Total 
 	amount_total = fields.Float(
-			'Total S/.', 
+			
+			'Total', 
+			
 			default='0', 
 
 			compute='_compute_amount_total', 
@@ -151,7 +175,9 @@ class Kardex(models.Model):
 
 			for move in record.stock_move_ids: 
 
-				total = total + move.product_uom_qty 
+				#total = total + move.product_uom_qty 
+				total = total + move.qty 
+
 
 			record.amount_total = total
 
--- models/kardex_move.py
+++ models/kardex_move.py
@@ -15,14 +15,83 @@ class KardexMove(models.Model):
 
 
 
+
+
+	location_dest = fields.Char(
+		)
+
+
+	location = fields.Char(
+		)
+
+
+
+
+
+
+	coeff = fields.Float(
+
+			default="1", 
+			
+			compute='_compute_coeff', 
+		)
+
+
+	@api.multi
+	def _compute_coeff(self):
+
+		for record in self:
+
+
+			if record.name != False:
+
+				#if 'OUT' in record.name: 
+				if 'OUT' in record.name  	or   'INT' in record.name: 
+					record.coeff = -1
+
+				else:
+					record.coeff = 1
+
+
+			elif record.location == 'Inventory loss' and record.location_dest == 'General': 
+				record.coeff = 1
+
+			elif record.location == 'General' and record.location_dest == 'Inventory loss': 
+				record.coeff = -1
+
+
+
+
+
+
+	qty = fields.Float(
+			'Quantity', 
+
+			compute='_compute_qty', 
+		)
+
+	@api.multi
+	def _compute_qty(self):
+
+		for record in self:
+
+			record.qty = record.product_uom_qty * record.coeff
+
+
+
+
+
+
+
+
 	name = fields.Char(
 		)
+
 
 
 	kardex_id = fields.Many2one(
 			'openhealth.kardex', 
 		)
-
 
 
 

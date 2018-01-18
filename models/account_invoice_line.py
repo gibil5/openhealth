db7bde7d192951b620c4189dfe302d93b0659024
gibil <jrevilla55@gmail.com>
2018-01-18 17:16:57 -0500

    ..

--- models/account_invoice_line.py
+++ models/account_invoice_line.py
@@ -26,3 +26,21 @@ class AccountInvoiceLine(models.Model):
 			default=1, 
 		)
 
+
+
+	#@api.multi
+	#def _get_analytic_line(self):
+	#	ref = self.invoice_id.number
+	#	return {
+	#		'name': self.name,
+	#		'date': self.invoice_id.date_invoice,
+	#		'account_id': self.account_analytic_id.id,
+	#		'unit_amount': self.quantity,
+	#		'amount': self.price_subtotal_signed,
+	#		'product_id': self.product_id.id,
+	#		'product_uom_id': self.uom_id.id,
+	#		'general_account_id': self.account_id.id,
+	#		'ref': ref,
+	#	}
+
+

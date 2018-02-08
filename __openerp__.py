5bb65a98d2b01381da208dc752150224b20f4ffd
gibil <jrevilla55@gmail.com>
2018-02-08 13:48:07 -0500

    ..

--- __openerp__.py
+++ __openerp__.py
@@ -376,13 +376,18 @@
 
 		# Stock 
 		'views/stock/stock.xml',
-
 		'views/stock/stock_picking.xml',
-		
 		'views/stock/stock_move.xml',
 		'data/stock_move_data.xml',
-
 		'views/stock/stock_move_selector.xml',
+
+
+
+		'views/stock/stock_inventory.xml',
+
+
+
+
 
 
 

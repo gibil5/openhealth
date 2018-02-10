78641841db590de16e2d3a69361c3244ab609530
gibil <jrevilla55@gmail.com>
2018-02-09 20:00:13 -0500

    ..

--- models/kardex_funcs.py
+++ models/kardex_funcs.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields, api
-import unidecode 
+#from openerp import models, fields, api
+#import unidecode 
 
 
 
-@api.multi
-def clean(self, foo):
-
-	bar = " ".join(foo.split())
-	bar = unidecode.unidecode(bar)
-
-	return bar  
+#@api.multi
+#def clean(self, foo):
+#	bar = " ".join(foo.split())
+#	bar = unidecode.unidecode(bar)
+#	return bar  

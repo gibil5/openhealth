c63a8c944837f9a1f4c6a97ce8bdb3855f8f2235
gibil <jrevilla55@gmail.com>
2018-02-10 14:23:53 -0500

    ..

--- __openerp__.py
+++ __openerp__.py
@@ -721,8 +721,8 @@
 		# ----------------------------------------------------------- Kardex ------------------------------------------------------
 
 		'views/stock/stock_move.xml',
-		#'data/stock_move_data.xml',
-		#'views/stock/stock_move_selector.xml',
+		'data/stock_move_data.xml',
+		'views/stock/stock_move_selector.xml',
 
 
 

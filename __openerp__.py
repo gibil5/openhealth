261918dfa5985fc53ea10152c9f8ccee78322e5a
gibil <jrevilla55@gmail.com>
2018-02-09 19:19:18 -0500

    ..

--- __openerp__.py
+++ __openerp__.py
@@ -373,14 +373,17 @@
 
 		# Stock 
 		'views/stock/stock.xml',
-		'views/stock/stock_picking.xml',
-		'views/stock/stock_move.xml',
-		'data/stock_move_data.xml',
-		'views/stock/stock_move_selector.xml',
-
-
-
 		'views/stock/stock_inventory.xml',
+
+
+
+		#'views/stock/stock_picking.xml',
+		#'views/stock/stock_move.xml',
+		#'data/stock_move_data.xml',
+		#'views/stock/stock_move_selector.xml',
+
+
+
 
 
 

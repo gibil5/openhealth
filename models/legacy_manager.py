4aeb1f45fabb5b5e114f9fd56720e748ec2ea412
gibil <jrevilla55@gmail.com>
2018-03-01 18:38:17 -0500

    ..

--- models/legacy_manager.py
+++ models/legacy_manager.py
@@ -127,7 +127,8 @@ class LegacyManager(models.Model):
  		#max_count = 200
  		#max_count = 1000
  		#max_count = 2000
- 		max_count = 5000
+ 		#max_count = 5000
+ 		max_count = 10000
  		#max_count = 20000
 
 

c832977265f7c6b061dd3c1d2033f61d8f67fe5a
gibil <jrevilla55@gmail.com>
2018-03-26 14:28:10 -0500

    ..

--- __openerp__.py
+++ __openerp__.py
@@ -753,17 +753,17 @@
 
 
 		# ----------------------------------------------------------- Security Rules ------------------------------------------------------
-		#'security/openhealth_security_readers.xml',
-		#'security/ir.rule.xml',
-		#'security/ir.model.access.csv',
+		'security/openhealth_security_readers.xml',
+		'security/ir.rule.xml',
+		'security/ir.model.access.csv',
 
 
 		# ----------------------------------------------------------- Products ------------------------------------------------------
 
 		# Products - Tmp 
-		#'data/prods/odoo_data_products.xml',			 
-		#'data/prods/odoo_data_products_new.xml',			
-		#'data/prods/odoo_data_services_co2.xml',		
+		'data/prods/odoo_data_products.xml',			 
+		'data/prods/odoo_data_products_new.xml',			
+		'data/prods/odoo_data_services_co2.xml',		
 		#'data/prods/odoo_data_services_exc.xml',		
 		#'data/prods/odoo_data_services_m22.xml',		
 		#'data/prods/odoo_data_services_med.xml',		

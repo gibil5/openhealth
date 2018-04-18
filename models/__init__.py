5421b2656284c91bb5be122ffea4a6e932df485f
gibil <jrevilla55@gmail.com>
2018-04-18 16:52:27 -0500

    ..

--- models/__init__.py
+++ models/__init__.py
@@ -3,56 +3,6 @@
 #from . import models
 
 
-# Deprecated ? 
-#from . import zone_category
-#from . import pathology_category
-
-
-# Highly Deprecated !
-#from . import order_report_nex_line
-#from . import quotation
-#from . import stock_picking 
-#from . import stock_pack_operation 
-#from . import purchase_po
-#from . import purchase_rfq
-#from . import purchase_line_rfq
-#from . import scheduler 
-#from . import sale_make_invoice_advance
-#from . import account_invoice_line
-#from . import sale_report_month
-#from . import sale_report_day 
-#from . import openhealth.models.appfuncs 
-#from openhealth.models import appfuncs 
-#from . import stock_move_selector_cremas
-#from . import stock_move_selector_consu
-#from . import stock_min_inventory
-#from . import stock_min_inventory_line
-#from . import zone_quick
-#from . import zone_co2
-#from . import zone_excilite
-#from . import zone_ipl
-#from . import zone_ndyag
-#from . import consultation_cos
-#from . import procedure_cos
-#from . import session_cos
-#from . import service_cosmetology
-#from . import appointment_cos
-#from . import stock_move_min
-#from . import stock_move_all
-#from . import kardex_move
-#from . import data
-#from . import physician_line 
-#from . import counter_raw
-#from . import image_full		# Deprecated ?
-#from . import multi_image		# Deprecated 
-#from . import legacy_patient
-#from . import legacy_order
-
-
-
-
-
-
 
 # Reports
 from . import report
@@ -61,24 +11,9 @@ from . import report_sale_product
 from . import report_order_line
 from . import product_counter 
 
-
-
 from . import sale_report
 from . import sale_report_md
-
-
-
-
-
-# Very important 
-from . import product_selector
-
 from . import order_report_nex
-
-from . import account_invoice
-
-
-
 
 
 
@@ -89,45 +24,30 @@ from . import patient
 from . import allergy  
 from . import product
 from . import image  			
-
-
 from . import counter
 
 
 
 
-
-
+# Legacy 
 from . import legacy
 from . import legacy_manager
 from . import legacy_manager_patient
 from . import legacy_manager_order
-
-
-
-
-
 from . import data_analyzer
 
 
 
 
-
-
-
-
-# After Travis
-
-
-
-
-
-
+# Purchase 
 from . import purchase
 from . import purchase_order_line
+from . import account_invoice
 
 
 
+
+# Stock 
 from . import stock_move
 from . import stock_min
 from . import stock_move_selector
@@ -135,15 +55,12 @@ from . import kardex
 
 
 
+
+# EMR
 from . import process 
 from . import treatment 
 from . import evaluation  
-
-
-
 from . import appointment 
-#from . import appfuncs 
-
 from . import consultation
 from . import recommendation
 from . import procedure
@@ -163,7 +80,6 @@ from . import pathology
 
 
 
-
 # Services 
 from . import service
 from . import service_co2
@@ -177,8 +93,10 @@ from . import service_vip
 
 
 
+# Order 
 from . import order
 from . import order_line
+from . import product_selector
 from . import payment_method
 from . import payment_method_line
 from . import event 
@@ -188,6 +106,7 @@ from . import card
 
 
 
+# Cosmetology 
 from . import cosmetology				
 from . import cos_classes
 

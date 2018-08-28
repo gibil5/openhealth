9b2db4decfb11364cc1e23ad26a3e26cdb6f35eb
gibil <jrevilla55@gmail.com>
2018-08-27 20:06:19 -0500

    ..

--- __openerp__.py
+++ __openerp__.py
@@ -523,7 +523,7 @@
 		# Patients - 2 
 		'views/patients/patient_personal.xml',
 		'views/patients/patient_control_docs.xml',
-		#'views/patients/patient_appointments.xml',
+		'views/patients/patient_appointments.xml',
 
 		# Appointments - 2
 		'views/appointments/calendar.xml',

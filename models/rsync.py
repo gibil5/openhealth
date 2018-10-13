106c7a635fd8db1cae9eec46304128519ba4400b
gibil <jrevilla55@gmail.com>
2018-10-13 14:01:10 -0500

    ..

--- models/rsync.py
+++ models/rsync.py
@@ -19,18 +19,14 @@ def synchronize():
 	print "Synchronize"
 	
 	# Init 
-	#base_dir = '/Users/gibil/Virtualenvs/Odoo9-min/odoo'
-	#path = base_dir + "/mssoft/ventas/" + export_date
+	base_dir = os.environ['HOME']
 
-	source = '/Users/gibil/Virtualenvs/Odoo9-min/odoo/mssoft/ventas/'
-	#destination = 'root@165.227.25.8:/root/mssoft/ventas'
+	#source = '/Users/gibil/Virtualenvs/Odoo9-min/odoo/mssoft/ventas/'
+	source = base_dir + "/mssoft/ventas/"
 	destination = 'root@165.227.25.8:/var/www/html/ventas'
 
 
 	# Rsync 
-	#os.system("rsync -rtv /opt/data/filename root@ip:/opt/data/file")
-	#os.system("rsync -rtv /Users/gibil/Virtualenvs/Odoo9-min/odoo/mssoft/ventas/ root@165.227.25.8:/root/mssoft/ventas")
-	#os.system("rsync -rv /Users/gibil/Virtualenvs/Odoo9-min/odoo/mssoft/ventas/ root@165.227.25.8:/root/mssoft/ventas")
 	os.system("rsync -rv " + source + " " + destination)
 
 

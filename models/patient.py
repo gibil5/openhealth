9003ccc89099f5fd9c7ac352756f2dad6feca464
gibil <jrevilla55@gmail.com>
2018-10-12 22:01:01 -0500

    ..

--- models/patient.py
+++ models/patient.py
@@ -26,6 +26,8 @@ class Patient(models.Model):
 
 # ----------------------------------------------------------- Dep ------------------------------------------------------
 	
+	appointment_ids = fields.Char()
+
 	x_service_quick_ids = fields.Char()
 
 	x_nr_quick_hands = fields.Integer()

5df2fed7d6c5a30c863c71bdca5b77d28861b964
gibil <jrevilla55@gmail.com>
2018-10-13 19:35:47 -0500

    ..

---
+++ web_calendar/readme.js
@@ -0,0 +1,61 @@
+
+// Read Me - Important 
+// --------------------
+
+
+// To add or remove a Doctor. In:
+//
+// static/lib/fullcalendar/js/fullcalendar.js
+
+
+
+
+
+// Draw verticals
+			document.getElementById("dr_canales").appendChild(jx_createLine(x0, 0, x0, y0));
+			document.getElementById("dr_escudero").appendChild(jx_createLine(x0, 0, x0, y0));
+			document.getElementById("dr_gonzales").appendChild(jx_createLine(x0, 0, x0, y0));
+			document.getElementById("dr_monteverde").appendChild(jx_createLine(x0, 0, x0, y0));
+			document.getElementById("end_right").appendChild(jx_createLine(x0, 0, x0, y0));
+
+
+
+
+// Draw labels
+				"<div class='fc-jx  fc-jx-1'	id='dr_chavarri'> 	Dr. Chavarri </div>"  +
+				"<div class='fc-jx  fc-jx-2'	id='dr_canales'> 	Dr. Canales </div>" +      
+				"<div class='fc-jx  fc-jx-3'	id='dr_escudero'> 	Dr. Escudero </div>" +
+				"<div class='fc-jx  fc-jx-4'	id='dr_gonzales'> 	Dr. Gonzales </div>" +
+				"<div class='fc-jx  fc-jx-5'	id='dr_monteverde'> 	Dr. Monteverde </div>" +  
+				"<div class='fc-jx  fc-jx-6'	id='end_right'> 		 		</div>" 
+
+
+
+
+// Set x
+			hash_left["CH"] = 	x[0];
+			hash_left["CA"] = 	x[1];
+			hash_left["ES"] = 	x[2];
+			hash_left["GO"] = 	x[3];
+			hash_left["MO"] = 	x[4];
+
+
+
+
+
+// Rodrigo Loayza 
+// Luis Castillo 
+// Carlos Abriojo
+dr_loayza
+dr_castillo
+dr_abriojo
+
+
+
+
+
+
+
+
+
+// 	/usr/lib/python2.7/site-packages/openerp/addons/web_calendar/static/lib/fullcalendar

7f79b2639f007dbad4132762acee9f3b1a2f42ab
gibil <jrevilla55@gmail.com>
2018-11-22 17:39:23 -0500

    ..

--- models/__init__.py
+++ models/__init__.py
@@ -154,7 +154,7 @@ from . import service_consultation
 # Order 
 from . import order
 from . import order_line
-from . import product_selector
+#from . import product_selector
 from . import payment_method
 from . import payment_method_line
 from . import event 

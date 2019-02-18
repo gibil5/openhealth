527052a5396df785321bbee92e80f536cca0a196
gibil <jrevilla55@gmail.com>
2019-02-17 23:28:11 -0500

    ..

--- __openerp__.py
+++ __openerp__.py
@@ -180,7 +180,7 @@
 
 		'data/prods/odoo_data_services_co2.xml',
 		'data/prods/odoo_data_services_exc.xml',
-		#'data/prods/odoo_data_services_m22.xml',
+		'data/prods/odoo_data_services_m22.xml',
 		'data/prods/odoo_data_services_med.xml',		
 		'data/prods/odoo_data_services_consult.xml',
 		'data/prods/odoo_data_services_cos.xml',

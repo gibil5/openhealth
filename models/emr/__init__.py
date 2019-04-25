4bb249d70e99acbb3c04093ed0a545f542b05267
gibil <jrevilla55@gmail.com>
2019-04-25 14:21:50 -0500

    ..

--- models/emr/__init__.py
+++ models/emr/__init__.py
@@ -41,14 +41,15 @@ from . import doctor
 #from . import pathology  			# Dep 2019
 
 # Services 
-from . import service
-from . import service_co2
-from . import service_excilite
-from . import service_ipl
-from . import service_ndyag
-from . import service_medical
-from . import service_quick
-from . import service_vip
-from . import service_cosmetology
-from . import service_product
-from . import service_consultation
+#from . import service
+#from . import service_co2
+#from . import service_excilite
+#from . import service_ipl
+#from . import service_ndyag
+#from . import service_medical
+#from . import service_quick
+#from . import service_vip
+#from . import service_cosmetology
+#from . import service_product
+#from . import service_consultation
+

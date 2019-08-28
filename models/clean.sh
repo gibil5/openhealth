7d046bbdf1c1f5c7698bb682ad6f1cd67f818481
gibil <jrevilla55@gmail.com>
2019-08-27 20:47:21 -0500

    ..

--- models/clean.sh
+++ models/clean.sh
@@ -1,13 +1,11 @@
 mv *.pyc ./pyc/. 
-mv libs/*.pyc ./pyc/.
 mv account/*.pyc ./pyc/.
 mv containers/*.pyc ./pyc/.
-mv patient/*.pyc ./pyc/.
+mv electronic/*.pyc ./pyc/.
+mv emr/*.pyc ./pyc/.
+mv libs/*.pyc ./pyc/.
 mv management/*.pyc ./pyc/.
 mv marketing/*.pyc ./pyc/.
-mv electronic/*.pyc ./pyc/.
 mv order/*.pyc ./pyc/.
-
-mv emr/*.pyc ./pyc/.
+mv patient/*.pyc ./pyc/.
 mv service/*.pyc ./pyc/.
-
